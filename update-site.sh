2c8738de0697fb83ac347036ac2e4dbd6fb7b743
Kartik Bedi <s2a3investmentholdings.com>
2026-01-17 22:02:42 -0500

    Auto update website

--- update-site.sh
+++ update-site.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-# Auto-update GitHub Pages
-
-cd "$(dirname "$0")"   # ensures the script runs in this folder
-git add .              # stage all changes
+# stage all changes
 git commit -am "Auto update website"  # commit automatically
 git push               # push to GitHub
-echo "Website updated! 🎉"
+echo "Website updated!  ~I"
 
